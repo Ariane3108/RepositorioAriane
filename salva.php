f859b4ada21eec4d1add040453be39650c5815f5
Ariane3108 <89281567+Ariane3108@users.noreply.github.com>
2021-12-04 13:16:29 -0300

    Add files via upload

---
+++ salva.php
@@ -0,0 +1,22 @@
+<?php
+
+$vNº=$_POST["">Nº<];
+$vData=$_POST["">Data<];
+$vFirma=$_POST["">Firma<];
+$vEndereço=$_POST["">Endereço<];
+$vComplemento=$_POST["">Complemento<];
+$vBairro=$_POST["">Bairro<];
+$vCidade=$_POST["">Cidade<];
+$vEstado=$_POST["">Estado<];
+$vCEP=$_POST["">CEP<];
+$vCNPJ=$_POST["">CNPJ<];
+$vInscrição=$_POST["">Inscrição<];
+$vTelefones=$_POST["">Telefones<];
+$vCelular=$_POST["">Celular<];
+$vE-mail=$_POST["">E-mail<];
+$vObservações=$_POST["">Observações<];
+
+echo "Nº: ".$vNº."<br/>Data: ".$vData."<br/>Firma: ".$vFirma."<br/>Endereço: ".$vEndereço."<br/>Complemento: ".$vEndereço."<br/>Complemento: ".$vComplemento."<br/>Bairro: ".$vBairro."<br/>Cidade: ".$vCidade."<br/>Estado: ".$vEstado."<br/>CEP: ".$vCEP."<br/>CNPJ: ".$vCNPJ."<br/>Inscrição: ".$vInscrição."<br/>Telefones: ".$vTelefones."<br/>Celular: ".$vCelular."<br/>E-mail: ".$vCelular."<br/>Observações: ".$vObservações."<br/>;
+
+
+?>
